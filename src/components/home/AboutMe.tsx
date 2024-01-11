0a4e069da3f09a368e7f437bc244f1dfbc442f50
furkanksl <furkankoseoglu65@gmail.com>
2024-01-11 23:15:18 +0300

    Updagte follow me section for mobile in about me section

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -1,6 +1,6 @@
 function AboutMe() {
     return (
-        <div className="flex flex-col md:flex-row justify-between items-center w-full gap-x-[40px] lg:gap-x-[120px]">
+        <div className="flex flex-col md:flex-row justify-between items-center w-full gap-y-5 md:gap-y-0 gap-x-[40px] lg:gap-x-[120px]">
             <div
                 id="me-wrapper"
                 className="flex relative group"
@@ -37,47 +37,49 @@ function AboutMe() {
             </div>
 
             <div className="flex flex-col items-start justify-start w-full z-10">
-                <div className="text-white text-[32px] font-bold font-syne">About me</div>
+                <div className="text-white text-[32px] font-bold font-syne mx-auto md:mx-0">About me</div>
                 <div className="text-secondary-dark text-lg font-normal font-rubik leading-7 tracking-wide pt-6">
                     Aaronn is a New York-based visual designer focusing on branding and visual identity. Her portfolio
                     showcases her wide range of work, spanning books, posters and web design.
                 </div>
                 <div className="text-secondary text-lg font-bold font-syne pt-8">follow me on:</div>
                 <div className="flex w-full justify-between pt-4">
-                    <div className="flex-1">
+                    <div className="flex-1 flex justify-start">
                         <a
                             href="https://github.com/furkanksl"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             Github
                         </a>
                     </div>
-                    <div className="flex-1">
+                    <div className="flex-1 flex justify-center">
                         <a
                             href="https://www.linkedin.com/in/furkanksl/"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             Linkedin
                         </a>
                     </div>
 
-                    <div className="flex-1">
+                    <div className="flex-1 flex justify-end">
                         <a
                             href="https://twitter.com/frknksglu"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             Twitter
                         </a>
                     </div>
                 </div>
 
-                <div className="text-white text-opacity-70 text-2xl font-bold font-syne mt-[100px]">my last posts</div>
+                <div className="text-white text-opacity-70 text-2xl font-bold font-syne mt-10 md:mt-[100px]">
+                    my last posts
+                </div>
                 <div className="cursor-pointer text-neutral-400 text-lg font-normal font-rubik underline tracking-wide">
                     How to check is array or not on type level
                 </div>
