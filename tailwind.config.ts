22a7c975a1bdfc5bea9f6de5a06bcc69bd959c40
furkanksl <furkankoseoglu65@gmail.com>
2024-01-11 23:04:05 +0300

    Add move animations into tailwind for hero pic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,7 @@ const config: Config = {
         },
         extend: {
             backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "my-pic": "url('/me.webp')",
             },
             colors: {
                 primary: "#1A1A1A",
@@ -22,6 +21,24 @@ const config: Config = {
                 "secondary-light": "#FF9142",
                 "secondary-dark": "#8D8D8D",
                 "secondary-darker": "#606060",
+            },
+            keyframes: {
+                "move-reversed-l": {
+                    "0%": { right: "0px" },
+                    "20%": { right: "-60px", transform: "rotate(90deg)" },
+                    "100%": { bottom: "0", top: "calc(100% - 85px)", right: "-60px", transform: "rotate(90deg)" },
+                },
+                "move-vertical": {
+                    "0%": {},
+                    "70%": { transform: "rotate(0deg)", left: "-32px" },
+                    "80%": { transform: "rotate(70deg)", left: "10px" },
+                    "100%": { bottom: "calc(100% - 90px)", transform: "rotate(90deg)", left: "90px" },
+                },
+            },
+            animation: {
+                "fade-in": "fade-in 0.5s ease-in-out",
+                "move-vertical": "move-vertical 1s forwards",
+                "move-reversed-l": "move-reversed-l 1s forwards",
             },
         },
     },
