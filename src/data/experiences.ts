a1d6618d681a94b17f355fe49116a9133ebdfedc
furkanksl <furkankoseoglu65@gmail.com>
2024-01-13 07:00:20 +0300

    Update experiences with motleylab

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -3,7 +3,7 @@ import { ExperienceType } from "@/types/ExperienceType";
 export const experiences: ExperienceType[] = [
     {
         company: "Dappd",
-        position: "Fullstack Engineer | TPM",
+        position: "Sr. Fullstack Engineer | TPM",
         companyWebsite: "https://dappd.net",
         startDate: "2022",
         endDate: "Present",
@@ -23,7 +23,8 @@ export const experiences: ExperienceType[] = [
             "AWS",
             "Github",
         ],
-        description: "",
+        description:
+            "I have worked on many projects from scratch to live. Built many apps and integrated many smart-contracts.",
         projects: [
             {
                 image: "/liquid-project.webp",
@@ -38,38 +39,24 @@ export const experiences: ExperienceType[] = [
         ],
     },
     {
-        company: "Dappd",
-        position: "Fullstack Engineer | TPM",
-        companyWebsite: "https://dappd.net",
-        startDate: "2022",
-        endDate: "Present",
-        techStack: [
-            "Next.js",
-            "TailwindCSS",
-            "TypeScript",
-            "Solidity",
-            "Hardhat",
-            "Ethers.js",
-            "Wagmi",
-            "React Query",
-            "Graphql",
-            "tRPC",
-            "Prisma",
-            "Postgresql",
-            "AWS",
-            "Github",
-        ],
-        description: "",
+        company: "Motley Labs",
+        position: "Fullstack Engineer (Contractor)",
+        companyWebsite: "https://twitter.com/MotleyLabs",
+        startDate: "May",
+        endDate: "Sept 2023",
+        techStack: ["Next.js", "TailwindCSS", "TypeScript", "Redux", "Solana", "SWR", "Prisma", "Golang", "Github"],
+        description:
+            "I have worked on nightmarket.io product which is main product of the company. Implemented UIs,cool animations and websockets. They ended up shutting down the company due to crypto winter.",
         projects: [
             {
-                image: "/liquid-project.webp",
-                name: "Liquid",
-                url: "https://www.getliquid.io/",
+                image: "/motleylab-2.webp",
+                name: "Motley Labs",
+                url: "https://twitter.com/nightmarketio",
             },
             {
-                image: "/statis-network-project.webp",
-                name: "Statis Network",
-                url: "https://www.stasis.network/",
+                image: "/motleylab-1.webp",
+                name: "Motley Labs",
+                url: "https://twitter.com/nightmarketio",
             },
         ],
     },
