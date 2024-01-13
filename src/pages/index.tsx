bd3b1ef13f44a5d8b22b23de293bf6237119469a
furkanksl <furkankoseoglu65@gmail.com>
2024-01-13 07:53:54 +0300

    Add id to expreinces section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <div className="flex flex-col max-w-[1240px] px-5 mt-[50px] md:mt-[110px] w-screen">
                 <AboutMe />
 
-                <div className="flex flex-col mt-[130px] md:mt-[168px] z-10 gap-y-3 md:gap-y-10">
+                <div id="experiences" className="flex flex-col mt-[130px] md:mt-[168px] z-10 gap-y-3 md:gap-y-10">
                     <div className="w-full h-[58px] justify-center items-center gap-8 flex flex-row">
                         <div className="w-[72px] h-[0px] border border-secondary"></div>
                         <div className="text-white text-3xl md:text-4xl font-normal font-syne">
