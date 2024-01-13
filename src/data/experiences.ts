b304f4c6c15547479933b1516f8bb7a4e0eee2bc
furkanksl <furkankoseoglu65@gmail.com>
2024-01-13 07:17:10 +0300

    Add shuttlescanner expereince

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,63 +1,88 @@
 import { ExperienceType } from "@/types/ExperienceType";
 
-export const experiences: ExperienceType[] = [
-    {
-        company: "Dappd",
-        position: "Sr. Fullstack Engineer | TPM",
-        companyWebsite: "https://dappd.net",
-        startDate: "2022",
-        endDate: "Present",
-        techStack: [
-            "Next.js",
-            "TailwindCSS",
-            "TypeScript",
-            "Solidity",
-            "Hardhat",
-            "Ethers.js",
-            "Wagmi",
-            "React Query",
-            "Graphql",
-            "tRPC",
-            "Prisma",
-            "Postgresql",
-            "AWS",
-            "Github",
-        ],
-        description:
-            "I have worked on many projects from scratch to live. Built many apps and integrated many smart-contracts.",
-        projects: [
-            {
-                image: "/liquid-project.webp",
-                name: "Liquid",
-                url: "https://www.getliquid.io/",
-            },
-            {
-                image: "/statis-network-project.webp",
-                name: "Statis Network",
-                url: "https://www.stasis.network/",
-            },
-        ],
-    },
-    {
-        company: "Motley Labs",
-        position: "Fullstack Engineer (Contractor)",
-        companyWebsite: "https://twitter.com/MotleyLabs",
-        startDate: "May",
-        endDate: "Sept 2023",
-        techStack: ["Next.js", "TailwindCSS", "TypeScript", "Redux", "Solana", "SWR", "Prisma", "Golang", "Github"],
-        description:
-            "I have worked on nightmarket.io product which is main product of the company. Implemented UIs,cool animations and websockets. They ended up shutting down the company due to crypto winter.",
-        projects: [
-            {
-                image: "/motleylab-2.webp",
-                name: "Motley Labs",
-                url: "https://twitter.com/nightmarketio",
-            },
-            {
-                image: "/motleylab-1.webp",
-                name: "Motley Labs",
-                url: "https://twitter.com/nightmarketio",
-            },
-        ],
-    },
-];
+const dappd: ExperienceType = {
+    company: "Dappd",
+    position: "Sr. Fullstack Engineer | TPM",
+    companyWebsite: "https://dappd.net",
+    startDate: "2022",
+    endDate: "Present",
+    techStack: [
+        "Next.js",
+        "TailwindCSS",
+        "TypeScript",
+        "Solidity",
+        "Hardhat",
+        "Ethers.js",
+        "Wagmi",
+        "React Query",
+        "Graphql",
+        "tRPC",
+        "Prisma",
+        "Postgresql",
+        "Firebase",
+        "AWS",
+        "Github",
+    ],
+    description:
+        "I have worked on many projects from scratch to live. Built many apps and integrated many smart-contracts.",
+    projects: [
+        {
+            image: "/liquid-project.webp",
+            name: "Liquid",
+            url: "https://www.getliquid.io/",
+        },
+        {
+            image: "/statis-network-project.webp",
+            name: "Statis Network",
+            url: "https://www.stasis.network/",
+        },
+    ],
+};
+
+const motleylabs: ExperienceType = {
+    company: "Motley Labs",
+    position: "Fullstack Engineer (Contractor)",
+    companyWebsite: "https://twitter.com/MotleyLabs",
+    startDate: "May",
+    endDate: "Sept 2023",
+    techStack: ["Next.js", "TailwindCSS", "TypeScript", "Redux", "Solana", "SWR", "Prisma", "Golang", "Github"],
+    description:
+        "I have worked on nightmarket.io product which is main product of the company. Implemented UIs,cool animations and websockets. They ended up shutting down the company due to crypto winter.",
+    projects: [
+        {
+            image: "/motleylab-2.webp",
+            name: "Motley Labs",
+            url: "https://twitter.com/nightmarketio",
+        },
+        {
+            image: "/motleylab-1.webp",
+            name: "Motley Labs",
+            url: "https://twitter.com/nightmarketio",
+        },
+    ],
+};
+
+const shuttlescanner: ExperienceType = {
+    company: "Shuttlescanner",
+    position: "Fullstack Engineer (part-time)",
+    companyWebsite: "https://shuttlescanner.web.app/",
+    startDate: "May",
+    endDate: "Sept 2023",
+    techStack: ["Angular", "TailwindCSS", "TypeScript", "Mysql", "Golang", "Firebase", "Git"],
+    description:
+        "Shuttlescanner is a young startup which is a different booking platform for other alternatives. I mostly worked on backend and some frontend. I implemented a lot of features and fixed a lot of bugs. I also implemented a lot of tests for the backend.",
+    projects: [
+        {
+            image: "/shuttlescanner-2.webp",
+            name: "Shuttlescanner",
+            url: "https://shuttlescanner.web.app/",
+        },
+        {
+            image: "/shuttlescanner-1.webp",
+            name: "Shuttlescanner",
+            url: "https://shuttlescanner.web.app/",
+        },
+    ],
+};
+
+export const experiences: ExperienceType[] = [dappd, motleylabs, shuttlescanner];
