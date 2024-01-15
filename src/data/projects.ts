e611aa4e59cd5beb63f77a212bc207a308da3255
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 05:35:02 +0300

    Create projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+import { ProjectType } from "@/types/ProjectType";
+
+export const projects: ProjectType[] = [
+    {
+        id: 1,
+        name: "ZUNOTE",
+        image: "/zunote.svg",
+        description:
+            "ZUNOTE is a multi-platform productivity app that you can take notes over voice recordings and set reminder to get notified at time. And it is open-source. I have launched the website and chrome-extension. Now working on desktop and mobile app.",
+        links: [
+            "https://zunote.io/",
+            "https://github.com/furkanksl/zunote-web",
+            "https://github.com/furkanksl/zunote-extension/releases/tag/0.1",
+        ],
+    },
+];
