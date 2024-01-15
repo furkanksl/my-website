036d18e988591db1a80e7808ed08ac07632f07cd
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 06:44:06 +0300

    Add id to projects section

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -3,7 +3,10 @@ import Project from "./Project";
 
 function Projects() {
     return (
-        <div className="flex flex-col justify-start items-start mt-[130px] md:mt-[168px] z-10 gap-y-3 md:gap-y-10">
+        <div
+            id="projects"
+            className="flex flex-col justify-start items-start mt-[130px] md:mt-[168px] z-10 gap-y-3 md:gap-y-10"
+        >
             <div className="w-full h-[58px] justify-center items-center gap-8 flex flex-row">
                 <div className="w-[72px] h-[0px] border border-secondary"></div>
                 <div className="text-white text-3xl md:text-[32px] font-bold font-syne">Projects</div>
