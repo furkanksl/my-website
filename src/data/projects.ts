b43267e433ccf768cedeb7ec097c8f21b0337e5d
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 06:07:44 +0300

    Add horae as project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ export const projects: ProjectType[] = [
     {
         id: 1,
         name: "ZUNOTE",
-        image: "/zunote.svg",
+        image: "/zunote.webp",
         description:
             "ZUNOTE is a multi-platform productivity app that you can take notes over voice recordings and set reminder to get notified at time. And it is open-source. I have launched the website and chrome-extension. Now working on desktop and mobile app.",
         links: [
@@ -12,5 +12,12 @@ export const projects: ProjectType[] = [
             "https://github.com/furkanksl/zunote-web",
             "https://github.com/furkanksl/zunote-extension/releases/tag/0.1",
         ],
+    },
+    {
+        id: 2,
+        name: "Horae",
+        image: "/horae.webp",
+        description: "A time tracking app to log hours of the day.",
+        links: ["https://gethorae.com/", "https://play.google.com/store/apps/details?id=com.categorize.time"],
     },
 ];
