69faf318a071ec61e281cf5a315ae393a9ee8250
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 21:22:33 +0300

    Fix project card style issues

--- src/components/home/Project.tsx
+++ src/components/home/Project.tsx
@@ -15,14 +15,14 @@ export default function Project(project: ProjectType) {
                     "z-10 flex px-3 w-[300px] h-[300px] bg-white/10 rounded-lg relative group shadow-[0_0px_5px_#FFFFFFC6] md:shadow-none hover:md:!shadow-[0_0px_20px_#FFFFFFC6] overflow-visible hover:scale-110 transition-all duration-300 bg-center bg-no-repeat bg-[length:100%_auto]"
                 )}
             >
-                <div className="z-20 opacity-0 group-hover:opacity-100 rounded-lg absolute top-0 bottom-0 right-0 left-0 h-full w-full backdrop-blur-md bg-white/5 transition-all delay-150"></div>
+                <div className="z-20 opacity-0 group-hover:opacity-100 invisible md:visible rounded-lg absolute top-0 bottom-0 right-0 left-0 h-full w-full backdrop-blur-md bg-white/5 transition-all delay-150"></div>
                 <div className="z-30 flex-col m-auto hidden md:flex max-h-[200px] h-0 group-hover:h-full overflow-y-auto opacity-0 group-hover:opacity-100 delay-100 transition-all text-center text-base text-secondary-darker font-rubik font-thin duration-500">
                     <p className="font-bold">{project.name}</p>
                     <p>{project.description}</p>
 
                     {/* {project.name + "\n" + project.description} */}
                 </div>
-                <div className="w-0 group-hover:w-12 rounded-r-lg absolute backdrop-blur-3xl hidden flex-col gap-y-3 opacity-0 group-hover:md:flex group-hover:opacity-100 group-hover:-right-12 bottom-0 right-0 top-0 h-[calc(100%_-_40px)] my-auto transition-all duration-500 delay-200 items-center justify-start py-2">
+                <div className="w-0 group-hover:w-12 rounded-r-lg absolute backdrop-blur-3xl hidden flex-col gap-y-3 opacity-0 md:flex group-hover:opacity-100 group-hover:-right-12 bottom-0 right-0 top-0 h-[calc(100%_-_40px)] my-auto transition-all duration-500 delay-200 items-center justify-start py-2">
                     {project.links.map((link, index) => (
                         <a
                             key={index}
