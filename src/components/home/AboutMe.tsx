c0621bf1e014d56e62c1511ff17db8de94b95d68
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 19:04:56 +0300

    Update about me section

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -48,58 +48,58 @@ function AboutMe() {
                     Senior Fullstack Engineer and exploring the AI field.
                 </div>
                 <div className="text-secondary text-lg font-bold font-syne pt-8">follow me on:</div>
-                <div className="flex w-full justify-between pt-4">
-                    <div className="flex-1 flex justify-start">
+                <div className="flex w-full justify-between pt-4 gap-x-1">
+                    <div className=" flex justify-start">
                         <a
                             href="https://github.com/furkanksl"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-rubik uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             Github
                         </a>
                     </div>
-                    <div className="flex-1 flex justify-center">
+                    <div className="flex justify-center">
                         <a
                             href="https://www.linkedin.com/in/furkanksl/"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-rubik uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             Linkedin
                         </a>
                     </div>
 
-                    <div className="flex-1 flex justify-end">
+                    <div className="flex justify-center items-center">
                         <a
                             href="https://twitter.com/frknksglu"
                             target="_blank"
                             referrerPolicy="no-referrer"
-                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min whitespace-nowrap text-neutral-400 text-base md:text-xl font-bold font-rubik uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
-                            Twitter
+                            X<span className="text-sm">(twitter)</span>
                         </a>
                     </div>
 
-                    <div className="flex-1 flex justify-center">
+                    <div className="flex justify-center">
                         <a
                             href="https://www.producthunt.com/@furkanksl"
                             target="_blank"
                             referrerPolicy="no-referrer"
                             title="Product Hunt"
-                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-['Inter'] uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
+                            className="w-min text-neutral-400 text-base md:text-xl font-bold font-rubik uppercase tracking-[2.06px] cursor-pointer hover:scale-110 hover:text-white"
                         >
                             PH
                         </a>
                     </div>
                 </div>
 
-                <div className="text-white text-opacity-70 text-2xl font-bold font-syne mt-10 md:mt-[100px]">
+                {/* <div className="text-white text-opacity-70 text-2xl font-bold font-syne mt-10 md:mt-[100px]">
                     my last posts
                 </div>
                 <div className="cursor-pointer text-neutral-400 text-lg font-normal font-rubik underline tracking-wide">
                     How to check is array or not on type level
-                </div>
+                </div> */}
             </div>
         </div>
     );
