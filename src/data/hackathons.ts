9cceb8eae42aade6de536ce05184d55a67f8d4ea
furkanksl <furkankoseoglu65@gmail.com>
2024-01-15 21:20:00 +0300

    Add data for hackathons

---
+++ src/data/hackathons.ts
@@ -0,0 +1,39 @@
+import { HackathonT } from "@/types/HackathonT";
+
+export const hackathons: HackathonT[] = [
+    {
+        id: 1,
+        name: "INSIDER - 'Sustainability'",
+        url: "https://kommunity.com/insider/events/insider-hackathon-surdurulebilirlik-2335fde0",
+        date: "2021",
+        achievement: "🏆 1st Place",
+    },
+    {
+        id: 2,
+        name: "VAKIFBANK - 'Hack to the Future'",
+        url: "https://www.vakifbank.com.tr/documents/basin_bultenleri/2021/VAKIFBANK_Hackathon_BB.pdf",
+        date: "2021",
+        achievement: "Finalist",
+    },
+    {
+        id: 3,
+        name: "EDUCATHON20",
+        url: "https://www.facebook.com/100057284956448/videos/212080563500425?__so__=permalink",
+        date: "2020",
+        achievement: "🥉 3rd Place",
+    },
+    {
+        id: 4,
+        name: "BTK - 'Smart Tech | IOT'",
+        url: "https://habitatdernegi.org/blog/akilli-teknolojiler-iot-hackathonunu/",
+        date: "2020",
+        achievement: "4rd Place",
+    },
+    {
+        id: 5,
+        name: "CORONATHON Turkey",
+        url: "http://tbv.org.tr/coronathon-turkiye-online-hackathon/",
+        date: "2020",
+        achievement: "60th/1000",
+    },
+];
